6e2ddd43df42253751e053147bf53257c62a7436
Paranjay Soni <soniparanjay@gmail.com>
2026-02-08 14:17:24 +0530

    Updated app.py

--- app.py
+++ app.py
@@ -72,3 +72,10 @@ if user_input:
 if st.sidebar.button("Clear Chat"):
     st.session_state.chat_history = []
     st.rerun()
+
+# Footer Credit
+st.markdown("---")
+st.markdown(
+    "<h5 style='text-align: center; color: grey;'>Built with  ❤️  by <a href='https://github.com/paranjaysoni' target='_blank' style='color: grey; text-decoration: none;'>Paranjay Soni</a></h5>",
+    unsafe_allow_html=True
+)
